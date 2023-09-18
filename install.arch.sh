5750a9ee9d4dace7c98c2a9cfb0c5f613e84c37d
Faustvii <Faustvii@users.noreply.github.com>
2023-09-18 22:57:02 +0200

    Added endavouros i3 configs

---
+++ install.arch.sh
@@ -0,0 +1,4 @@
+yay -S easyeffects lsp-plugins-lv2 calf zam-plugins-lv2 mda.lv2
+yay -S discord flatpak librewolf-bin spotify code lazygit
+yay -S steam heroic-games-launcher-bin protonup-qt lutris mangohud gamemode
+yay -S --needed nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
