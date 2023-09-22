a9ef873c16e6a6351325e315d411cc9ce1e619a2
Faustvii <Faustvii@users.noreply.github.com>
2023-09-23 00:06:16 +0200

    Automated commit: Sat Sep 23 00:06:16 CEST 2023

--- sync.sh
+++ sync.sh
@@ -6,12 +6,12 @@ LC_TIME="C.UTF-8" # English 24 hour format
 CONFIG_DIR="$HOME/Git/dotfiles"
 PUSH_INTERVAL=600 # 10 minutes in seconds 
 
-eval $(ssh-agent)
+eval "$(ssh-agent)"
 ssh-add
 
 # Function to push changes to the Git repo
 push_changes() {
-  cd "$CONFIG_DIR"
+  cd "$CONFIG_DIR" || exit
   git add .
   git commit -m "Automated commit: $(date)"
   git fetch
